d5e8437e9c8ed456e8911e33df02aabe604012dc
Klaral <klaral@users.noreply.github.com>
2017-12-18 13:15:22 +0100

    first commit

---
+++ rullavagn.js
@@ -0,0 +1,31 @@
+/**************** SLIDE JQUERY ********************************************************/
+
+$(function(){
+                $('.slideContent img:gt(0)').hide();
+                setInterval(function(){
+                  $('.slideContent :first-child').hide()
+                     .next('img').show()
+                     .end().appendTo('.slideContent');}, 
+                  3000);
+            });
+
+
+/**************** POPUP/LIGHTBOX JQUERY ************************************************/
+
+$(function() {
+    //----- OPEN
+    $('[data-popup-open]').on('click', function(e)  {
+        var targeted_popup_class = jQuery(this).attr('data-popup-open');
+        $('[data-popup="' + targeted_popup_class + '"]').fadeIn(350);
+ 
+        e.preventDefault();
+    });
+ 
+    //----- CLOSE
+    $('[data-popup-close]').on('click', function(e)  {
+        var targeted_popup_class = jQuery(this).attr('data-popup-close');
+        $('[data-popup="' + targeted_popup_class + '"]').fadeOut(350);
+ 
+        e.preventDefault();
+    });
+});
